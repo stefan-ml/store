3b3f48907e8ea6e405339a8cf27182228b647e0d
Stefan <mladenovic.stefan1997@gmail.com>
2023-12-07 21:38:30 +0100

    Fix Basket microservice

--- Store.Service.Basket/Program.cs
+++ Store.Service.Basket/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Service.Basket.Services;
+using Store.Service.Basket.DbContexts;
+using Store.Service.Basket.Repositories;
+using Store.Service.Basket.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<IBasketLinesRepository, BasketLinesRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IBasketChangeProductRepository, BasketChangeProductRepository>();
+
+builder.Services.AddDbContext<BasketDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnDbStr"));
+});
 
 var app = builder.Build();
 
