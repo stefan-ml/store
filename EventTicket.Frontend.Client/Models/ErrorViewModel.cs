580c2e9efc096a200cdec6fdc5871edc364841a7
Stefan <mladenovic.stefan1997@gmail.com>
2023-12-30 12:26:56 +0100

    Added frontend project

---
+++ EventTicket.Frontend.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EventTicket.Frontend.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
